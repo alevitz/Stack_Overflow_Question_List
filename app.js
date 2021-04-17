a0b8121f9de26ba4da4f765ddefe2bd5080cb1b8
Adam <adamlevitz@gmail.com>
2021-04-16 17:30:33 -0700

    get data to populate on table

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 
 let table = $('#table');
 
-let questionData = async function(){
-  
+let questionData = async function () {
+
   let data = await axios.get("https://api.stackexchange.com/2.2/questions?page=1&order=desc&sort=activity&site=stackoverflow");
 
-  data.data.items
-    
+  for (let item = 0; item < data.data.items.length; item++) {
+    let author = data.data.items[item].owner.display_name;
+    let title = data.data.items[item].title;
+    let creation_date = data.data.items[item].creation_date;
+
+    let row = $("<tr>");
+
+    row.append($(`<td>${author}</td>`))
+      .append($(`<td>${title}</td>`))
+      .append($(`<td>${creation_date}</td>`));
+
+    table.append(row);
+
+  }
+
   console.log(data);
 }
 
